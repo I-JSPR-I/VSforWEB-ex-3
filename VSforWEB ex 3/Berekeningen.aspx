9bb2081436db35acc2ce066fefa574587f52607c
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-05-18 13:38:37 +0200

    Add project files.

---
+++ VSforWEB ex 3/Berekeningen.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Berekeningen.aspx.cs" Inherits="VSforWEB_ex_3.Berekeningen" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="Value1" runat="server"></asp:TextBox>
+        <p>
+            <asp:TextBox ID="Value2" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="btnBereken" runat="server" OnClick="btnBereken_Click" Text="BEREKEN" />
+        <br />
+        <asp:Label ID="Som" runat="server" Text="De Som"></asp:Label>
+        <br />
+        <asp:Label ID="Verschil" runat="server" Text="Het Verschil"></asp:Label>
+        <br />
+        <asp:Label ID="Product" runat="server" Text="Het Product"></asp:Label>
+        <br />
+        <asp:Label ID="quotient" runat="server" Text="Het quotient"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ VSforWEB ex 3/Berekeningen.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VSforWEB_ex_3
+{
+    public partial class Berekeningen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Page.IsPostBack)
+            {
+                Som.Text = Convert.ToString(Convert.ToInt32(Value1.Text) + Convert.ToInt32(Value2.Text));
+                Verschil.Text = Convert.ToString(Convert.ToInt32(Value1.Text) - Convert.ToInt32(Value2.Text));
+                Product.Text = Convert.ToString(Convert.ToInt32(Value1.Text) * Convert.ToInt32(Value2.Text));
+                quotient.Text = Convert.ToString(Math.Round(Convert.ToDouble(Value1.Text) / Convert.ToDouble(Value2.Text), 2, MidpointRounding.AwayFromZero));
+            }
+        }
+
+        protected void btnBereken_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
